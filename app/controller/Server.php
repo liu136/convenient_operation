ee2391ee7714246014661a0fe736b8319a19a515
liuyifan <1364483885@qq.com>
2020-05-22 14:52:38 +0800

    no message

--- app/controller/Server.php
+++ app/controller/Server.php
@@ -13,8 +13,9 @@ class Server extends BaseController
         $pull_data_file = 'Destination_IP=' . $all->ip . "\r\nTarget_database_name=" . $all->database. "\r\nlocal_Database=" . $all->local_database . "\r\nactorid=" . $all->actor_id . "\r\nvirtual=" . $all->virtual;
         $pull_data_file_name = time();
 
-        file_put_contents('/home/getData/dataSh/'. $pull_data_file_name .'.sh', $pull_data_file);
+        file_put_contents('/home/www/dataSh/'. $pull_data_file_name .'.sh', $pull_data_file);
 
+        shell_exec('scp -p 9669 /home/www/dataSh/.' . $pull_data_file_name .'.sh' . ' root@122.114.222.172 /home/getData/dataSh');
         $res = shell_exec('ssh -p 9669 root@122.114.222.172 bash /home/getData/main.sh ' . $pull_data_file_name . '.sh');
 
         return $res;
