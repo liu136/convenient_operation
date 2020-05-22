7b5ba594f42b6283af2a54d8469a35acdeb20669
liuyifan <1364483885@qq.com>
2020-05-22 10:33:08 +0800

    repair

--- app/controller/Server.php
+++ app/controller/Server.php
@@ -7,10 +7,16 @@ class Server extends BaseController
 {
     public function pullData()
     {
-        echo 1; exit;
-        $cmd = 'type a.txt';
-        $res = shell_exec($cmd);
-        print_r($res);
-        return 1;
+        $all = file_get_contents('php://input');
+        $all = json_decode($all);
+
+        $pull_data_file = 'Destination_IP=' . $all->ip . "\r\nTarget_database_name=" . $all->database. "\r\nlocal_Database=" . $all->local_database . "\r\nactorid=" . $all->actor_id . "\r\nvirtual=" . $all->virtual;
+        $pull_data_file_name = time();
+
+        file_put_contents('./home/getData/dataSh/'. $pull_data_file_name .'.sh', $pull_data_file);
+
+        $res = shell_exec('ssh -p 9669 root@122.114.222.172 bash /home/getData/main.sh ' . $pull_data_file_name . '.sh');
+
+        return $res;
     }
 }
