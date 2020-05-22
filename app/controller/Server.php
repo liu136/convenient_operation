073ff21fc38754141f7b63a15d57f16c2b3f10af
liuyifan <1364483885@qq.com>
2020-05-22 14:58:56 +0800

    no message

--- app/controller/Server.php
+++ app/controller/Server.php
@@ -13,10 +13,14 @@ class Server extends BaseController
         $pull_data_file = 'Destination_IP=' . $all->ip . "\r\nTarget_database_name=" . $all->database. "\r\nlocal_Database=" . $all->local_database . "\r\nactorid=" . $all->actor_id . "\r\nvirtual=" . $all->virtual;
         $pull_data_file_name = time();
 
+        // 生成的拉取数据文件
         file_put_contents('/home/www/dataSh/'. $pull_data_file_name .'.sh', $pull_data_file);
 
-        shell_exec('scp -p 9669 /home/www/dataSh/.' . $pull_data_file_name .'.sh' . ' root@122.114.222.172 /home/getData/dataSh');
+        shell_exec('scp -p 9669 /home/www/dataSh.' . $pull_data_file_name .'.sh' . ' root@122.114.222.172 /home/getData/dataSh');
         $res = shell_exec('ssh -p 9669 root@122.114.222.172 bash /home/getData/main.sh ' . $pull_data_file_name . '.sh');
+
+        // 删除生成的拉取数据文件
+        shell_exec('rm -rf /home/www/dataSh/' . $pull_data_file_name .'.sh');
 
         return $res;
     }
