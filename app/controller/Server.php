a51a68c7de250c3342f7b7937ed627095313969e
liuyifan <1364483885@qq.com>
2020-06-12 15:49:17 +0800

    no message

--- app/controller/Server.php
+++ app/controller/Server.php
@@ -13,7 +13,7 @@ class Server extends BaseController
         $all = json_decode($all);
 
         // 生成 拉取 数据
-        $pull_data_file = 'Destination_IP=' . $all->ip . "\r\nTarget_database_name=" . $all->database. "\r\nlocal_Database=" . $all->local_database . "\r\nactorid=" . $all->actor_id . "\r\nvirtual=" . $all->virtual;
+        $pull_data_file = 'Destination_IP=' . $all->ip . "\nTarget_database_name=" . $all->database. "\nlocal_Database=" . $all->local_database . "\nactorid=" . $all->actor_id . "\nvirtual=" . $all->virtual;
         // 生成拉取数据文件名
         $pull_data_file_name = time();
 
@@ -21,10 +21,11 @@ class Server extends BaseController
         file_put_contents('/home/www/dataSh/'. $pull_data_file_name .'.sh', $pull_data_file);
 
         // 移动拉取数据文件到指定服务器
-        shell_exec('scp -P 9669 /home/www/dataSh/' . $pull_data_file_name .'.sh' . ' root@122.114.222.172:/home/getData/dataSh 2>&1 &');
+        $r = shell_exec('sshpass -p\'8(GuangNai)6*6(WangLuo)8*v*!@#123\' scp -P 9669 /home/www/dataSh/' . $pull_data_file_name .'.sh' . ' root@122.114.222.172:/home/getData/dataSh 2>&1 &');
+//        shell_exec('scp -P 9669 /home/www/dataSh/' . $pull_data_file_name .'.sh' . ' root@122.114.222.172:/home/getData/dataSh 2>&1 &');
 //        exec("rsync -avz --progress -e 'ssh -p 9669' " . "/home/www/dataSh/" . $pull_data_file_name .".sh" ." 122.114.222.172:/home/getData/dataSh", $res, $status);
         // 执行拉取数据 shell 脚本
-        $res = shell_exec('ssh -p 9669 root@122.114.222.172 bash /home/getData/main.sh ' . $pull_data_file_name . '.sh 2>&1 &');
+        $res = shell_exec('sshpass -p\'8(GuangNai)6*6(WangLuo)8*v*!@#123\' ssh -p 9669 root@122.114.222.172 bash /home/getData/main.sh ' . $pull_data_file_name . ' 2>&1 &');
 
         // 删除生成的拉取数据文件
         shell_exec('rm -rf /home/www/dataSh/' . $pull_data_file_name .'.sh 2>&1 &');
