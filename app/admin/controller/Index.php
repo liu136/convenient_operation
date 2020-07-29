270de7e0a8e7724e5234ac38aac8e67a87fe9b08
liuyifan <1364483885@qq.com>
2020-07-29 14:37:25 +0800

    no message

---
+++ app/admin/controller/Index.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Kris Liu
+ * Date: 2020/7/28
+ * Time: 20:01
+ */
+namespace app\admin\controller;
+
+class Index
+{
+    public function uploadImage()
+    {
+        return 'my';
+    }
+}
