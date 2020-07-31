3a152feade7eb133e9e3a93d51b94df0925b83d9
liuyifan <1364483885@qq.com>
2020-07-31 20:31:31 +0800

    no message

---
+++ script/server/WebSocket.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Kris Liu
+ * Date: 2020/7/31
+ * Time: 20:24
+ */
+
+class WebSocket
+{
+    const HOST = '0.0.0.0';
+    const POST = '9501';
+
+    public $ws = null;
+
+    public function __construct()
+    {
+        $this->ws = new Swoole\WebSocket\Server(self::HOST, self::POST);
+
+        $this->ws->on('open', array($this, 'onOpen'));
+        $this->ws->on('message', array($this, 'onMessage'));
+        $this->ws->on('close', array($this, 'onClose'));
+    }
+
+    public  function onOpen($server, $request)
+    {
+        print_r($server);
+        print_r($request);
+    }
+
+    public  function onMessage($server, $frame)
+    {
+        print_r($frame);
+    }
+
+    public  function onClose($server, $fd, $reactorId)
+    {
+        print_r($fd. '-----' . $reactorId);
+    }
+}
